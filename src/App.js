0b4c86399e438ac5fcfda52fc13bc779f9887ac4
Your Name <you@example.com>
2021-02-17 10:16:34 +1100

    Init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,90 @@
 import React from 'react';
 import './App.css'
-import Yo from './yo.jpeg'
-function App() {
+import {signInWithGoogle} from './components/firebase/firebase.utils'
+
+import {auth, userProfile, saveToFireStore, firestore, getTheTestObjFromFireStore, updateTheTestObjInsideFireStore, deleteTheTestObjInsideFireStore} from './components/firebase/firebase.utils'
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      currentUser:null,
+      signInStatus:"Google",
+      item:''
+    }
+    this.handleChange=this.handleChange.bind(this)
+    this.handleDelete=this.handleDelete.bind(this)
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.handleUpdate=this.handleUpdate.bind(this)
+  }
+  test=null;
+  unsubscribeFromAuth=null
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      this.setState({currentUser:user},()=>{console.log(this.state)})
+      if (user) {
+        this.setState({signInStatus:"Sign Out"})
+        // const userAuth= await this.state.currentUser
+        userProfile(user)
+      }
+      else if (!user){
+        this.setState({signInStatus:"Google"})
+      }
+      // const userAuth= await this.state.currentUser
+      // console.log(userAuth.uid)
+      // userProfile(user)
+    })
+    const ref=firestore.collection('yoyo')
+    this.test=ref.onSnapshot(
+      async function heyhey(snapshot){
+        console.log(snapshot);
+        const refMap=getTheTestObjFromFireStore(snapshot)
+        console.log(refMap)
+      }
+    )
+  }
+  componentWillUnmount(){
+    this.unsubscribeFromAuth()
+  }
+  handleChange= async (e)=>{
+    const item = e.target.value
+    this.setState({item:item})
+  }
+  handleSubmit=(e)=>{
+    e.preventDefault()
+    const dummyCollectionRef="yoyo"
+    saveToFireStore(dummyCollectionRef,this.state.currentUser)
+  }
+  handleUpdate=(e)=>{
+    e.preventDefault()
+    const dummyUpdate="heyo"
+    updateTheTestObjInsideFireStore(this.state.currentUser,dummyUpdate)
+  }
+  handleDelete=(e)=>{
+    e.preventDefault()
+    deleteTheTestObjInsideFireStore(this.state.currentUser)
+  }
+  render(){
+  const signIn = this.state.signInStatus
   return (
     <div className="App">
-      <h1 className='title'>단원증</h1>
-      <h2>Y&C 임신부</h2>
-      <img width="250px" src={Yo} alt="profile"/>
-      <h5>소속:본사</h5>
-      <h1>구 예 지</h1>
-      <span>YE JI GOO</span>
+      <div className='yo'>
+      <h1 className='title'>Search Here</h1>
+      <form >
+        <input type='text' name='item' onChange={this.handleChange}/>
+        <button type='submit'>Search</button>
+      </form>
+      <a href='/'>All Jobs</a>
+      <button onClick={signInWithGoogle}>{signIn}</button>
+      <button onClick={this.handleSubmit}>Set Test</button>
+      <button onClick={()=>auth.signOut()}>sign out</button>
+      <button onClick={this.handleUpdate}>Update Test</button>
+      <button onClick={this.handleDelete}>Delete Test</button>
+      </div>
     </div>
+    
   );
+  }
 }
 
 export default App;
---
+++ src/components/Job-Page/JobPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './JobPage.scss';
+
+function JobPage (){
+    return(
+        <div className='JobPage'>
+            <h1>Job Title</h1>
+            <span>Job Description</span>
+            <span>Address</span>
+            <span>Contact Number</span>
+        </div>
+    )
+}
+export default JobPage
